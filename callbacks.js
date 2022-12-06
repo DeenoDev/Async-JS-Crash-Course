76b9ad3c55a04d4eec2a87617be673ccbd7b4b3f
DeenoDev <deyanoklaasen@outlook.com>
2022-12-06 22:57:47 +0200

    getPosts function added to callbacks,js

--- callbacks.js
+++ callbacks.js
@@ -2,3 +2,15 @@ const posts = [
     { title: 'Post One', Body: 'This is post one'},
     { title: 'Post Two', Body: 'This is post two'},
 ];
+
+function getPosts(){
+    setTimeout(()=> {
+      let output = '';
+      posts.forEach((post, index)=> {
+        output += `<li>${post.title}</li>`
+      });
+      document.body.innerHTML = output; 
+    }, 1000);
+}
+
+getPosts();
